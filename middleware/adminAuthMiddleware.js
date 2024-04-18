0f5207273ec14ff0bd0a1a84d1e6b4d6def1fe9f
nurislammridha <mridha851@gmail.com>
2024-04-18 19:10:42 +0600

    Update

--- middleware/adminAuthMiddleware.js
+++ middleware/adminAuthMiddleware.js
@@ -10,21 +10,24 @@ const protect = async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(" ")[1];
   }
-
+  console.log('token', token)
   if (!token) {
+
     return next(new ErrorResponse("Not authorized to access this route", 401));
   }
-  const decoded = jwt.verify(token, "123456789");
-  console.log(decoded)
+  // const decoded = jwt.verify(token, "123456789");
+  // console.log(decoded)
   try {
 
     const decoded = jwt.verify(token, "123456789");
 
-    if (decoded.email === "support@sellkon.com") {
-      return next(new ErrorResponse("No user found with this id", 404));
+    if (decoded.email !== "support@sellkon.com") {
+      return next(new ErrorResponse("Admin email invalid", 404));
+    } else if (decoded.password !== "sellkon@#2024COM") {
+      return next(new ErrorResponse("Admin password incorrect", 404));
     }
 
-    req.user = user;
+    // req.user = user;
 
     next();
   } catch (err) {
